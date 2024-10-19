c4f34e7da5a57aeafbadf8f9a1d130ba77d48e87
Jacco <34662810+prosir@users.noreply.github.com>
2024-10-19 14:08:49 +0200

    easier to read fixed fxmanifest. Removed bs

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-author 'Kakarot'
+author 'Kakarot & Prosir'
 description 'Allows players to create multiple characters'
-version '1.2.0'
+version '1.0.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
@@ -26,11 +26,7 @@ ui_page 'html/index.html'
 
 files {
     'html/index.html',
-    'html/style.css',
-    'html/reset.css',
-    'html/vue.js',
-    'html/swal2.js',
-    'html/profanity.js'
+    'html/*.js',
 }
 
 dependencies {
